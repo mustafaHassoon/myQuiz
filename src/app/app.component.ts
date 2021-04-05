eebf76f30ab83a0d613848a09e4226d41236a244
mustafahassoon@gmail.com <mustafahassoon@gmail.com>
2021-04-05 23:25:27 +0200

    task 3.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,48 @@
 import { Component } from '@angular/core';
-import { QuestionsService } from './questions.service';
-import { Quiz, Answers, Choice } from './quiz.model';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>'
 })
-export class AppComponent {
-  answers: Answers;
-  quiz: Quiz;
-  currentQuestionIndex: number;
-  showResults = false;
 
-  constructor(private questionsService: QuestionsService) {
-    this.questionsService.getJSON('french').subscribe(data => {
+export class AppComponent { }
 
-      this.quiz = new Quiz('french', data);
-      this.answers = new Answers();
-      this.currentQuestionIndex = 0;
-    });
-  }
 
-  updateChoice(choice: Choice) {
-    this.answers.values[this.currentQuestionIndex] = choice;
-  }
 
-  nextOrViewResults() {
-    if (this.currentQuestionIndex === this.quiz.questions.length - 1) {
-      this.showResults = true;
-      return;
-    }
+// import { Component } from '@angular/core';
+// import { QuestionsService } from './questions.service';
+// import { Quiz, Answers, Choice } from './quiz.model';
 
-    this.currentQuestionIndex++;
-  }
-}
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss']
+// })
+// export class AppComponent {
+//   answers: Answers;
+//   quiz: Quiz;
+//   currentQuestionIndex: number;
+//   showResults = false;
+
+//   constructor(private questionsService: QuestionsService) {
+//     this.questionsService.getJSON('french').subscribe(data => {
+
+//       this.quiz = new Quiz('french', data);
+//       this.answers = new Answers();
+//       this.currentQuestionIndex = 0;
+//     });
+//   }
+
+//   updateChoice(choice: Choice) {
+//     this.answers.values[this.currentQuestionIndex] = choice;
+//   }
+
+//   nextOrViewResults() {
+//     if (this.currentQuestionIndex === this.quiz.questions.length - 1) {
+//       this.showResults = true;
+//       return;
+//     }
+
+//     this.currentQuestionIndex++;
+//   }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
-import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { QuestionFormComponent } from './question-form/question-form.component';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 
+
+import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
-  bootstrap: [AppComponent],
+  declarations: [AppComponent, QuestionFormComponent, ResultsComponent, QuestionsComponent, WelcomeComponent],
   imports: [
-    // other imports ...
+    BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    BrowserModule
-  ],
-  declarations: [AppComponent, QuestionFormComponent],
-  providers: [],
+    RouterModule.forRoot(appRoutes),
+    NgbModule,
+    [BrowserAnimationsModule],
 
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
 
